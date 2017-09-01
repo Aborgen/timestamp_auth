d86566e284ec4dfdd1fd34f68945f4b82ac36750
Alexander Borgen <AlexanderBorgen@gmail.com>
2017-09-01 16:09:30 -0700

    Add '/header' route that gets and displays client data

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // Dependencies
 const express          = require('express'),
-      expressValidator = require('express-validator'),
       path             = require('path'),
       bodyParser       = require('body-parser'),
       router           = express.Router(),
@@ -11,6 +10,8 @@ const express          = require('express'),
       app              = express(),
       port             = 3000;
 
+// Initialize express
+app.enable('trust proxy');
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
@@ -18,15 +19,30 @@ app.use(express.static(path.join(__dirname, "public")));
 // Body-parser Middleware setup
 app.use(bodyParser.json());
 
+// Home
 app.get('/', (req, res) => {
-    res.send("HELLO");
+    res.send("WELCOME");
 });
 
-app.get('/:arg', (req, res) => {
+// Get UIC and 'natural' time located in :arg
+app.get('/convert/:arg', (req, res) => {
     console.log(req.path);
     const hurm = scripts.parseArgument(req.path.substr(1));
     console.log(hurm);
     res.json(hurm);
+});
+
+// Get user details
+app.get('/header', (req, res) => {
+    const ipaddress = req.ip || req.headers['x-forwarded-for'],
+          language  = req.headers['accept-language'].split(',')[0],
+          software  = req.headers['user-agent'].split(/[()]/)[1];
+    res.json(
+        {
+            ipaddress,
+            language,
+            software
+        });
 });
 
 // Start server
